c9d4bf738c3e95812a47c5a20181d5b75209fe3d
DESKTOP-557UREB\Ivy <ivytsai2@gmail.com>
2022-10-05 17:22:16 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.80d50364e6e92b03a9f27c2e5f4b5963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "902d0de553d5db095675",
+    "url": "/ivytsai2/COMP3104/static/css/main.99c00238.chunk.css"
+  },
+  {
+    "revision": "902d0de553d5db095675",
+    "url": "/ivytsai2/COMP3104/static/js/main.902d0de5.chunk.js"
+  },
+  {
+    "revision": "60d4301ff0f7992257b4",
+    "url": "/ivytsai2/COMP3104/static/js/runtime~main.60d4301f.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/ivytsai2/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "6a36f93d6b88675d1df1ebe50705b3b7",
+    "url": "/ivytsai2/COMP3104/index.html"
+  }
+];
